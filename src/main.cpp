92000ce7ce43eef7dd37556e23e08a122bf0fd41
HiddenAbility <hopeme950@gmail.com>
2022-12-02 20:36:29 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor motor(10,pros::E_MOTOR_GEAR_GREEN,false,pros::E_MOTOR_ENCODER_DEGREES);
+	pros::Motor motor(10,pros::E_MOTOR_GEAR_GREEN,0,pros::E_MOTOR_ENCODER_DEGREES);
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
