0dfce11fe7d503da5375a69647a9228520dcbdd2
HiddenAbility <hopeme950@gmail.com>
2023-02-06 15:27:20 -0500

    roller rework

--- src/functions.cpp
+++ src/functions.cpp
@@ -2,43 +2,39 @@
 #include "pros/misc.h"
 #include "robot.hpp"
 
+// toggles the extension piston
 void extension() {
-
   jerry.set_value(!extensionActivated);
   extensionActivated = !extensionActivated;
 }
+
+// uses a boolean input to toggle extension
 void extension(bool input) {
   if (input) {
     extension();
   }
 }
 
-// function to actuate drivetrain. uses 6 vs 8 motors depending on status of
-// pto.
+// function to move the drivetrain based on controller input
 void update_drivetrain() {
   prosLDM = prosController.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
   prosRDM = prosController.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
 }
 
-// function to actuate the roller mech. uses up and down arrows to spin the
+// function to actuate the roller mech uses up and down arrows to spin the
 // roller mech
 void roll_roller() {
   rM = 100 * (prosController.get_digital(pros::E_CONTROLLER_DIGITAL_L1) -
               prosController.get_digital(pros::E_CONTROLLER_DIGITAL_L2));
 }
 
-void spinRoller() {
-  okapiLDM.moveVoltage(-4000);
-  okapiRDM.moveVoltage(-4000);
+// spins the roller a certain distance
+void spinRoller(double desiredAngle) {
+  move(4_in);
+  rMotor.moveRelative(ROLLER_RATIO * desiredAngle, 600);
   pros::delay(500);
-  rMotor.moveVoltage(12000);
-  pros::delay(250);
-  rMotor.moveVoltage(0);
-  okapiLDM.moveVoltage(4000);
-  okapiRDM.moveVoltage(4000);
-  pros::delay(500);
-  okapiLDM.moveVoltage(0);
-  okapiRDM.moveVoltage(0);
+  move(-1_in);
 }
 
+// moves the robot a certain distance.
 void move(okapi::QLength distance) { chassis->moveDistance(-distance); }
